52164f7fade147c2d8f5681efa76d7b81460ffd5
ana96ps <paulinha_960@hotmail.com>
2026-04-06 22:34:04 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert (Preparar, Agir, Verificar)
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
-
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  await page.getByTestId('search-order-id').click()
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-MVRT5P')
   await page.getByTestId('search-order-button').click()
-  await page.getByTestId('search-order-button').click()
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MVRT5P')
+
+  //Assert
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
